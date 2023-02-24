0c28e7840ae93063cbf4fafc7867bf548d3a9c05
ljoonal <ljoonal@ljoonal.xyz>
2023-02-24 18:57:06 +0200

    Forbid unsafe code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
+#![forbid(unsafe_code)]
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
