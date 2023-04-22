8e8abaa1d3422fc51a110ba4a4723ea77b19c91e
ljoonal <5854483+ljoonal@users.noreply.github.com>
2023-04-22 22:55:45 +0300

    V6

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
       includeAllSources: true,
       reporters: [
         { type: "html" },
-        { type: "json-summary" },
+        { type: "cobertura" },
         { type: "text-summary" },
       ],
     },
